56eb1d7ab35751f349f062f9f4016af7a370dd5d
hugodecasta <hugodecasta@orange.fr>
2020-02-03 16:58:53 +0100

    updated system with new simpler map

--- git_updater.js
+++ git_updater.js
@@ -10,92 +10,93 @@ if(!fs.existsSync(map_path)) {
     console.log('Systems map "'+map_path+'" missing')
     process.exit(1)
 }
+let sys_map = JSON.parse(fs.readFileSync(map_path,'utf8'))
 
-// -------------------------------------------------------------- FUNCTIONS
+// -------------------------------------------------------------- PROCESS
 
-function rebase_sys_data(sys_data) {
+let processes = {}
 
-    let sys_data_base = {
-        dir:'.',
-        executor:'node',
-        script:'.',
-        options:'',
-        init_commands:[],
-        update_secs:10
-    }
-    let final_sys_data = sys_data_base
-    for(let prop in sys_data) {
-        final_sys_data[prop] = sys_data[prop]
-    }
-    return final_sys_data
+// --------
+
+function start_process(name) {
+
+    console.log(name,'start')
+
+    let desc = sys_map.process[name]
+
+    processes[name] = null
+
+    let exec_command = desc.exec
+    let exec_sp = exec_command.split(' ')
+    let process = child_process.spawn(exec_sp[0],exec_sp.slice(1))
+    process.on('exit',function(code) {
+        console.log(name,'exit',code)
+    })
+
+    processes[name] = process
 }
 
-// -------------------------------------------------------------- CORE
+// --------
 
-let systems_map = JSON.parse(fs.readFileSync(map_path,'utf8'))
+function setup_process(name) {
 
-for(let sys_name in systems_map) {
+    console.log(name,'setup')
 
-    // --------------- LOGGER
+    let desc = sys_map.process[name]
 
-    function log() {
-        console.log('['+sys_name+']',Array.from(arguments).join(' '))
+    let setups = desc.setup_procs
+    for(let com of setups) {
+        child_process.execSync(com)
     }
+}
 
-    // --------------- LOADING
+// --------
 
-    let sys_data = rebase_sys_data(systems_map[sys_name])
+function stop_process(name) {
 
-    let git_connect = sys_data.git_connect
-    let git_repo = sys_data.git_repo
-    let dir = sys_data.dir
-    let init_commands = sys_data.init_commands
-    let executor = sys_data.executor
-    let script = sys_data.script
-    let options = sys_data.options
-    let update_secs = sys_data.update_secs
+    if(!(name in processes)) {
+        return
+    }
+    console.log(name,'kill')
 
-    let git_dir = dir+'/'+git_repo
+    processes[name].kill('SIGINT')
+}
 
-    let git_clone_options = ['clone',git_connect+'/'+git_repo,git_dir]
-    let git_pull_options = ['-C',git_dir,'pull']
+// -------------------------------------------------------------- REPO
 
-    // --------------- INIT COMMANDS
+function launch_repo(name) {
 
-    function init() {
-        log('init commands ...')
-        for(let init_com of init_commands) {
-            log('-',init_com)
-            let com_sp = init_com.split(' ')
-            let com = com_sp[0]
-            let opt = com_sp.slice(1)
-            child_process.spawnSync(com,opt)
+    let desc = sys_map.repo[name]
+
+    let git = desc.git
+    let dir = desc.dir
+    let procs = desc.processes
+
+    let repo_name = git.split('/')
+    repo_name = repo_name[repo_name.length-1]
+
+    let repo_dir = dir+'/'+repo_name
+
+    // --- meths
+
+    function setup_procs() {
+        for(let proc of procs) {
+            setup_process(proc)
         }
-        log('init done !')
     }
 
-    // --------------- CLONE ?
-
-    let exec_option = [script].concat(options.split(' '))
-    if(!fs.existsSync(git_dir)) {
-        log('clone repo first ...')
-        child_process.spawnSync('git',git_clone_options)
-        log('repo cloned')
-        init()
+    function start_procs() {
+        for(let proc of procs) {
+            start_process(proc)
+        }
     }
 
-    // --------------- UPDATER
-
-    async function update_system() {
-
-        let updater = child_process.spawn('git',git_pull_options)
-
+    async function is_updated() {
+        let updater = child_process.spawn('git',['-C',repo_dir,'pull'])
         let full_data = []
-
         updater.stdout.on('data', function (data) {
             full_data.push(data.toString())
         });
-        
         return new Promise(ok=>{
             updater.on('exit',function(code) {
                 ok(full_data.length > 1)
@@ -103,41 +104,37 @@ for(let sys_name in systems_map) {
         })
     }
 
-    // --------------- LAUNCHER
-
-    let exec = null
-
-    function restart_system() {
-
-        log('starting system')
-        if(exec != null) {
-            log('closing first')
-            exec.kill('SIGINT')
-        }
-
-        exec = child_process.spawn(executor,exec_option)
-
-        exec.stdout.on('data', function (data) {
-            log('{DATA}:',data.toString())
-        });
-        
-        exec.on('exit',function(code) {
-            log('{EXIT}:',code)
-        })
-    }
-
-    // --------------- CORE
-
-    async function updater() {
-        let must_restart = await update_system()
-        if(must_restart) {
-            log('must restart after update')
-            init()
-            restart_system()
+    function stop_procs() {
+        for(let proc of procs) {
+            stop_process(proc)
         }
     }
 
-    restart_system()
-    setInterval(updater,update_secs*1000)
+    // --- clone ?
 
+    if(!fs.existsSync(repo_dir)) {
+        console.log('cloning',repo_name)
+        child_process.execSync('git clone '+git+' '+repo_dir)
+        setup_procs()
+    }
+
+    // --- procedure
+
+    setInterval(async function() {
+        if(await is_updated()) {
+            console.log(name,"updated !")
+            stop_procs()
+            start_procs()
+        }
+    },5000)
+}
+
+// -------------------------------------------------------------- CORE
+
+for(let proc in sys_map.process) {
+    start_process(proc)
+}
+
+for(let repo in sys_map.repo) {
+    launch_repo(repo)
 }
